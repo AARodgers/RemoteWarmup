ab3cab7544a3c010d2d9da80f6fe628060874752
github username <github email address>
2019-10-29 16:04:48 -0600

    renders to json

--- db/migrate/20191029154337_create_students.rb
+++ db/migrate/20191029154337_create_students.rb
@@ -1,7 +1,9 @@
 class CreateStudents < ActiveRecord::Migration[6.0]
   def change
     create_table :students do |t|
-
+      t.string :name
+      t.integer :age
+      t.integer :grade
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+
+students = Student.create(
+    [
+    {name: 'Student1', age: 16, grade: 11}, 
+    {name: 'Student2', age: 17, grade: 12}, 
+    {name: 'Student3', age: 16, grade: 11}, 
+    {name: 'Student5', age: 15, grade: 10}, 
+    {name: 'Student6', age: 18, grade: 12}, 
+    {name: 'Student7', age: 16, grade: 11}, 
+    {name: 'Student8', age: 17, grade: 12},
+    {name: 'Student9', age: 16, grade: 10},
+    {name: 'Student10', age: 15, grade: 9}
+    ]
+)
